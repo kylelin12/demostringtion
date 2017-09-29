2df207fd3313adab2d6bc605fc59259894a42ede
Kyle Lin <klin12@stuy.edu>
2017-09-29 15:29:08 -0400

    Added address tracking

--- demo.c
+++ demo.c
@@ -8,10 +8,13 @@ int main() {
     char number_demo[] = "12345678"; // Part of string to append demo
     
     printf("string before strcat: \"%s\"\n", string); // "Foo"
-    
+    printf("address of string: %p\n\n", string);
+
     strcat(string, add);
     printf("string after strcat:  \"%s\"\n", string); // "Foo, Bar"
-    
+    printf("address of string: %p\n\n", string);
+
     strncat(string, number_demo, 4); // strcat first 4 chars of number_demo
     printf("string after strncat: \"%s\"\n", string); // "Foo, Bar1234"
+    printf("address of string: %p\n\n", string);
 }
